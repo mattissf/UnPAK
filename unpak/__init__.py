3939c1fce6d46455daa1dbeb8677265faa08409c
Mattis Stordalen Flister <mattis@altibox.no>
2013-05-29 09:09:05 +0200

    Improved readability

--- unpak/__init__.py
+++ unpak/__init__.py
@@ -2,51 +2,79 @@ import time
 import struct
 import os
 
-def extract(pak_file_path, destination_root_dir=None, verbose=None):
-	pak_file = open(pak_file_path, 'rb')
+SIGNATURE_POSITION = 0
+SIGNATURE_LENGTH = 4
 
-	if verbose:
-		pak_file.seek(0)
-		signature = pak_file.read(4)
-		print "Signature is " + str(signature)
+DIRECTORY_OFFSET_POSITION = 4
+DIRECTORY_OFFSET_LENGTH = 4
 
-	pak_file.seek(4)
-	directory_offset = struct.unpack('i', pak_file.read(4))[0]
-	directory_length = struct.unpack('i', pak_file.read(4))[0]
+DIRECTORY_SIZE_POSITION = 8
+DIRECTORY_SIZE_LENGTH = 4
 
-	if verbose:
-		print "Directory offset: " + str(directory_offset)
-		print "Directory length: " + str(directory_length)
+DIRECTORY_ENTRY_LENGTH = 64
+DIRECTORY_ENTRY_FILE_PATH_LENGTH = 56
+DIRECTORY_ENTRY_FILE_OFFSET_LENGTH = 4
+DIRECTORY_ENTRY_FILE_LENGTH = 4
 
-	files_in_directory = directory_length / 64
+def unpack_integer(binary_value):
+    return struct.unpack('i', binary_value)[0]
 
-	print "Extracting {0} files in archive".format(files_in_directory)
+def unpack_null_terminated_string(binary_value):
+    return binary_value.split(b'\x00')[0]
 
-	for directory_entry in xrange(files_in_directory):
-		pak_file.seek(directory_offset + (directory_entry * 64))
+def extract(pak_file_path, unpack_root_dir=None, verbose=None):
+    pak_file = open(pak_file_path, 'rb')
 
-		file_path = pak_file.read(56).split(b'\x00')[0]
-		file_position_in_archive = struct.unpack('i', pak_file.read(4))[0]
-		file_length_in_archive = struct.unpack('i', pak_file.read(4))[0]
+    if verbose:
+        pak_file.seek(SIGNATURE_POSITION)
+        signature = pak_file.read(SIGNATURE_LENGTH)
+        print "Signature is " + str(signature)
 
-		path_components = os.path.split(file_path)
+    pak_file.seek(DIRECTORY_OFFSET_POSITION)
+    directory_offset_in_binary = pak_file.read(DIRECTORY_OFFSET_LENGTH)
+    directory_offset = unpack_integer(directory_offset_in_binary)
 
-		directories = os.path.join(*path_components[:-1])
-		basename = path_components[-1]
+    pak_file.seek(DIRECTORY_SIZE_POSITION)
+    directory_size_in_binary = pak_file.read(DIRECTORY_SIZE_LENGTH)
+    directory_size = unpack_integer(directory_size_in_binary)
 
-		destination_dir = os.path.join(destination_root_dir, directories)
-		
-		print "Extracting '{0}' to directory '{1}'".format(
-			basename,
-			destination_dir,
-		)
+    if verbose:
+        print "Directory offset: " + str(directory_offset)
+        print "Directory length: " + str(directory_size)
 
-		if not os.path.isdir(destination_dir):
-			os.makedirs(destination_dir)
+    entries_in_directory = directory_size / DIRECTORY_ENTRY_LENGTH 
 
-		pak_file.seek(file_position_in_archive)
-		file_contents = pak_file.read(file_length_in_archive)
-		with open(os.path.join(destination_dir, basename), 'wb') as output_file:
-			output_file.write(file_contents)
+    print "Extracting {0} files in archive".format(entries_in_directory)
 
-	print "Done extracting archive"
+    for directory_entry in xrange(entries_in_directory):
+        pak_file.seek(directory_offset + (directory_entry * DIRECTORY_ENTRY_LENGTH))
+
+        file_path_in_binary = pak_file.read(DIRECTORY_ENTRY_FILE_PATH_LENGTH)
+        file_path = unpack_null_terminated_string(file_path_in_binary)
+
+        file_offset_in_archive_in_binary = pak_file.read(DIRECTORY_ENTRY_FILE_OFFSET_LENGTH)
+        file_offset_in_archive = unpack_integer(file_offset_in_archive_in_binary)
+
+        file_length_in_binary = pak_file.read(DIRECTORY_ENTRY_FILE_LENGTH)
+        file_length = unpack_integer(file_length_in_binary)
+
+        file_path_components = os.path.split(file_path)
+        file_path_directories = os.path.join(*file_path_components[:-1])
+        file_path_basename = file_path_components[-1]
+
+        unpack_dir = os.path.join(unpack_root_dir, file_path_directories)
+        
+        print "Extracting '{0}' to directory '{1}'".format(
+            file_path_basename,
+            unpack_dir,
+        )
+
+        if not os.path.isdir(unpack_dir):
+            os.makedirs(unpack_dir)
+
+        pak_file.seek(file_offset_in_archive)
+        file_contents = pak_file.read(file_length)
+        with open(os.path.join(unpack_dir, file_path_basename), 'wb') as output_file:
+            output_file.write(file_contents)
+
+    print "Done extracting archive"
